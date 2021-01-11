d1e1e1fd2dd162eb5704f4c8f80a93ccf4ba340c
Chris Dent <chris@indented.co.uk>
2021-01-11 19:34:33 +0000

    Fixes installation when module path does not exist

--- Indented.ChocoPackage/templates/chocolateyInstall.ps1
+++ Indented.ChocoPackage/templates/chocolateyInstall.ps1
@@ -7,17 +7,20 @@ $destinations = switch ($true) {
 
 foreach ($destination in $destinations) {
     $psEditionPath = Join-Path -Path $env:PROGRAMFILES -ChildPath $destination
-    if (Test-Path -Path $psEditionPath) {
-        $modulePath = Join-Path -Path $psEditionPath -ChildPath '%MODULE_NAME%'
 
-        if (Test-Path -Path $modulePath) {
-            $source = Join-Path -Path $PSScriptRoot -ChildPath '%MODULE_NAME%\*'
-        } else {
-            $source = Join-Path -Path $PSScriptRoot -ChildPath '%MODULE_NAME%'
-        }
-        Copy-Item -Path $source -Destination $modulePath -Recurse -Force
-
-        Join-Path -Path $modulePath -ChildPath '%MODULE_VERSION%\chocolateyInstalled.txt' |
-            New-Item -Path { $_ } -ItemType File
+    if (-not (Test-Path $psEditionPath)) {
+        New-Item -Path $psEditionPath -ItemType Directory
     }
+
+    $modulePath = Join-Path -Path $psEditionPath -ChildPath '%MODULE_NAME%'
+
+    if (Test-Path -Path $modulePath) {
+        $source = Join-Path -Path $PSScriptRoot -ChildPath '%MODULE_NAME%\*'
+    } else {
+        $source = Join-Path -Path $PSScriptRoot -ChildPath '%MODULE_NAME%'
+    }
+    Copy-Item -Path $source -Destination $modulePath -Recurse -Force
+
+    Join-Path -Path $modulePath -ChildPath '%MODULE_VERSION%\chocolateyInstalled.txt' |
+        New-Item -Path { $_ } -ItemType File
 }
